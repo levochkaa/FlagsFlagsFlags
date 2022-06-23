1bdf749760ab3e6601e2e012ada4974b6040e954
Lev Poznyakov <levfirstgamer@mail.ru>
2022-06-23 21:09:19 +0300

    Initial commit

--- FlagsFlagsFlags/ViewController.swift
+++ FlagsFlagsFlags/ViewController.swift
@@ -2,13 +2,32 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
+    var countries = [String]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        title = "FlagsFlagsFlags"
+        navigationController?.navigationBar.prefersLargeTitles = true
+        countries += ["estonia", "france", "germany", "ireland", "italy", "monaco", "nigeria", "poland", "russia", "spain", "uk", "us"]
     }
 
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return countries.count
+    }
 
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Country", for: indexPath)
+        cell.textLabel?.text = countries[indexPath.row].uppercased()
+        cell.imageView?.image = UIImage(named: countries[indexPath.row])
+        return cell
+    }
+
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if let vc = storyboard?.instantiateViewController(withIdentifier: "Detail") as? DetialViewController {
+            vc.selectedImage = countries[indexPath.row]
+            navigationController?.pushViewController(vc, animated: true)
+        }
+    }
 }
 
