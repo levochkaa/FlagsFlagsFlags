7f93cf890ee1a704fb007dde6b3d517a01290426
Lev Poznyakov <levfirstgamer@mail.ru>
2022-07-15 14:58:34 +0300

    Added spacing between rows

--- FlagsFlagsFlags/ViewController.swift
+++ FlagsFlagsFlags/ViewController.swift
@@ -18,8 +18,14 @@ class ViewController: UITableViewController {
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Country", for: indexPath)
+        let verticalPadding: CGFloat = 8
+        let maskLayer = CALayer()
+        maskLayer.backgroundColor = UIColor.systemBackground.cgColor
+        maskLayer.frame = CGRect(x: cell.bounds.origin.x, y: cell.bounds.origin.y, width: cell.bounds.width, height: cell.bounds.height).insetBy(dx: 0, dy: verticalPadding / 2)
+
         cell.textLabel?.text = countries[indexPath.row].uppercased()
         cell.imageView?.image = UIImage(named: countries[indexPath.row])
+        cell.layer.mask = maskLayer
         return cell
     }
 
